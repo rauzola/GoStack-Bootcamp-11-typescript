3ed5a77a8d7f76b6e9a7d5651f065e570b5edbf8
rauzola <raul_sigoli@hotmail.com>
2020-07-07 09:45:20 -0300

    Tipando Objetos e Vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Response, Request } from "express";
+import createUser from "./services/CreateUser";
+
+// string, number, boolean, object, Arry
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "raul_sigoli@hotmail.com",
+    password: "123456",
+    techs: [
+      "Node.js",
+      "ReactJS",
+      "React Native",
+      { title: "Javascript", experience: 100 },
+    ],
+  });
+
   return response.json({ message: "Hello World" });
 }
